753b97d944e5f34705b2a93a3a441a04092183e8
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-10-20 00:59:19 +0500

    feat: add 05 step

---
+++ 05-event-handling/src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  import Smiley from "./Smiley.svelte";
+  import Container from "./Container.svelte";
+  import Header from "./Header.svelte";
+  import Button from "./Button.svelte";
+
+  let say = false;
+  let showHeader = false;
+
+  setTimeout(() => {
+    say = true;
+    showHeader = false;
+  }, 1000);
+</script>
+
+{#if showHeader}
+  <Header />
+{/if}
+
+<Container>
+  {#if say}
+    <div>Hi!</div>
+  {:else if !say}
+    Not saying anything
+  {/if}
+
+  {#each [2, 1, 0] as index}
+    <Smiley {index} size={index + 1} />
+  {/each}
+
+  <Button on:click={() => (showHeader = !showHeader)}>Show Header</Button>
+</Container>
+
+<style>
+  div {
+    color: red;
+  }
+  :global(*) {
+    box-sizing: border-box;
+  }
+  :global(body, html) {
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
+  }
+</style>
---
+++ 05-event-handling/src/Container.svelte
@@ -0,0 +1,17 @@
+<div class="Container">
+  <slot />
+</div>
+
+<style>
+  .Container {
+    height: 100vh;
+    width: 100vw;
+    font-family: "Segoe UI", sans-serif;
+    border: solid black 20px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
