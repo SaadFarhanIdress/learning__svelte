a6aff399bc4760bbae695df6033f17a52b45b8e7
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-10-22 14:25:10 +0500

    feat: add 07 step

---
+++ 07-reactivity/src/Buttons.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let buttons = [];
+</script>
+
+<button on:click={() => dispatch("clickHeaderToggler", true)}> Show </button>
+
+<button on:click={() => dispatch("clickHeaderToggler", false)}> Hide </button>
+
+{#each buttons as button}
+  <button on:click={() => dispatch("clickStory", button)}>
+    {button.text}
+  </button>
+{/each}
+
+<style>
+  button {
+    font-family: "Nunito", sans-serif;
+    display: block;
+    font-size: 1.2rem;
+    margin: 10px;
+    transition: 1s;
+    padding: 0.7em 1em;
+    cursor: pointer;
+    border-radius: 50px;
+    background-color: #40b3ff;
+    color: white;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+      0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  }
+</style>
